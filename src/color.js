ca2b3b08e122686c3a0ae9db20c9bf291b575a73
cinephile <aprilkb412@naver.com>
2023-01-20 20:05:21 +0900

    ui: 메인 & 홈 제작

--- src/color.js
+++ src/color.js
@@ -1,4 +1,3 @@
 export const BACKGROUND_COLOR = "#fef6ec";
 export const POINT_COLOR = "#e37b58";
-export const PROJECTNAME_FRONT_COLOR = "#000000";
-export const PROJECTNAME_BACK_COLOR = "#c93636";
+export const PROJECT_COLOR = "#c93636";
