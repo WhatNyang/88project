dc2230ba04f06ca87f43c5b3dbedb7f2e31270aa
HongguLim <Honggu.Lim@outlook.com>
2023-01-20 16:39:19 +0900

    feat: add mypage

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -2,14 +2,16 @@ import Main from "../pages/Main";
 import Header from "../components/Header";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Detail from "../pages/Detail";
+import Mypage from "../pages/Mypage";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Main />}></Route>
+        {/* <Route path="/" element={<Main />}></Route> */}
         <Route path="/:id" element={<Detail />}></Route>
+        <Route path="/" element={<Mypage />}></Route>
       </Routes>
     </BrowserRouter>
   );
