1515a18a4b9929a25cedcf2cf22782d279c68f15
be-brain <chha5232@gmail.com>
2023-01-20 16:00:59 +0900

    feat/add-color

---
+++ color.js
@@ -0,0 +1,4 @@
+export const BACKGROUND_COLOR = "#fef6ec";
+export const POINT_COLOR = "#e37b58";
+export const PROJECTNAME_FRONT_COLOR = "#000000";
+export const PROJECTNAME_BACK_COLOR = "#c93636";
