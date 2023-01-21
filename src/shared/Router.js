daa4965fa64f81d5301592275d38b5262032c320
Jinsu <jjs0517@naver.com>
2023-01-21 16:03:22 +0900

    login/UI

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Detail from "../pages/Detail";
 import Home from "../pages/Home";
-
+import Login from "../pages/Login";
 const Router = () => {
   return (
     <BrowserRouter>
@@ -12,6 +12,7 @@ const Router = () => {
         <Route path="/index" element={<Home />} />
         <Route path="/" element={<Main />} />
         <Route path="/:id" element={<Detail />} />
+        <Route exact path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
