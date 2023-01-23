131491d6d37e14926aed3b8796580a19bb3b0422
hotsix <codinggo@naver.com>
2023-01-24 02:20:56 +0900

    feat: 수정 버튼 누르면 닉네임 변경 가능

--- src/components/mypage/MypageProfile.tsx
+++ src/components/mypage/MypageProfile.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import React, { useState } from "react";
+import React, { ChangeEvent, useRef, useState } from "react";
 
 const imgProfile = "img/image.jpg";
 type ProfileItemProps = {
@@ -9,36 +9,84 @@ type ProfileItemProps = {
   review: number;
 };
 function MypageProfile() {
-  const [profile, setProfile] = useState<ProfileItemProps>();
+  const initialState = {
+    nickname: "나의 별명",
+    image: imgProfile,
+    like: 10,
+    review: 2,
+  };
+  const [profile, setProfile] = useState<ProfileItemProps>(initialState);
   const BaseProfile = profile?.image || imgProfile;
+  const [editmode, setEdit] = useState(false);
+  const profileEdit = () => {
+    setEdit(!editmode);
+  };
+  const nameRef = useRef<HTMLInputElement>(null);
+  const profileEditComplete = () => {
+    const img = null || profile.image;
+    const ProfileItem = {
+      nickname: nameRef.current?.value,
+      image: img,
+      review: 2,
+    };
+    nameRef.current?.value &&
+      setProfile({ ...profile, nickname: nameRef.current?.value });
+
+    setEdit(!editmode);
+  };
+
   return (
     <StyledDivOne>
       <div></div>
       <div>
         <ProfileImage img={BaseProfile}></ProfileImage>
-        <button>
-          {" "}
-          <label htmlFor="changeimg">파일선택</label>
-        </button>
-
-        <input hidden id="changeimg" type="file" placeholder="파일선택"></input>
+        {editmode ? (
+          <>
+            <button>
+              {" "}
+              <label htmlFor="changeimg">파일선택</label>
+            </button>
+            <input
+              hidden
+              id="changeimg"
+              type="file"
+              placeholder="파일선택"
+            ></input>
+          </>
+        ) : (
+          ""
+        )}
       </div>
       <div></div>
       <ProfileList>
-        <ProfileNickname>나의 이름</ProfileNickname>
+        {editmode ? (
+          <ProfileNicknameEdit defaultValue={profile?.nickname} ref={nameRef} />
+        ) : (
+          <>
+            {" "}
+            <ProfileNickname>{profile?.nickname}</ProfileNickname>
+          </>
+        )}
+
         <ProfileListLikeReview>
           <ProfileListLike>
             <div></div>
             <div>관심</div>
-            <div>0</div>
+            <div>{initialState.like}</div>
           </ProfileListLike>
           <ProfileListReview>
             <div>리뷰</div>
-            <div>0</div>
+            <div>{initialState.review}</div>
           </ProfileListReview>
         </ProfileListLikeReview>
       </ProfileList>
-      <ProfileEditbButton>수정하기</ProfileEditbButton>
+      {editmode ? (
+        <ProfileEditButton onClick={profileEditComplete}>
+          완료하기
+        </ProfileEditButton>
+      ) : (
+        <ProfileEditButton onClick={profileEdit}>수정하기</ProfileEditButton>
+      )}
     </StyledDivOne>
   );
 }
@@ -50,7 +98,8 @@ const StyledDivOne = styled.div`
 const ProfileImage = styled.div<{ img: string }>`
   width: 100px;
   height: 100px;
-  background-size: cover;
+  /* background-repeat: no-repeat; */
+  background-size: contain;
   background-image: url(${(props) => props.img});
 `;
 const ProfileList = styled.div`
@@ -76,7 +125,7 @@ const ProfileListReview = styled.div`
   font-size: small;
   text-align: center;
 `;
-const ProfileEditbButton = styled.button`
+const ProfileEditButton = styled.button`
   font-size: small;
   text-align: center;
   border: 0;
@@ -87,6 +136,17 @@ const ProfileEditbButton = styled.button`
   :hover {
     background-color: #e37b58;
     transition: 0.7s;
+  }
+`;
+const ProfileNicknameEdit = styled.input`
+  border: none;
+  width: 100px;
+  height: 30px;
+  padding: 1px;
+  :hover {
+    background-color: #e37b58;
+    transition: 0.7s;
+    border: none;
   }
 `;
 export default MypageProfile;
