d918f137bcd159197092bdc12e12d06194a13b02
cinephile <aprilkb412@naver.com>
2023-01-23 22:12:54 +0900

    hotfix: 에러 수정

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyB5xX9z_mcn_bnWdLw5UHC7uSh6FLGW2l0",
   authDomain: "whatnyang-b00f1.firebaseapp.com",
@@ -14,6 +12,6 @@ const firebaseConfig = {
   appId: "1:939703503768:web:86075cc0aa7976e08d39ed",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const dbService = getFirestore(app);
+export const authService = getAuth(app);
